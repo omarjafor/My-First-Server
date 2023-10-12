681ec9158cb10e8b603fa023baa11ed2102d3eaf
omarjafor <omarjaforchy@gmail.com>
2023-10-12 12:58:08 +0600

    server working

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.get('/', (req, res) => {
     res.send('Hello From My First Server')
 })
 
-
+app.get('/data', (req, res) => {
+    res.send('More data coming........')
+})
 
 app.listen(port, () => {
     console.log(`My first server running on ${port}`);
