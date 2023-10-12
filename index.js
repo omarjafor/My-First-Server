c627aab254ef17632813308c78173c40a2508421
omarjafor <omarjaforchy@gmail.com>
2023-10-12 13:05:31 +0600

    server data load

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const data = require('./data.json');
 const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello From My First Server')
+})
+
+app.get('/donations', (req, res) => {
+    res.send(data);
 })
 
 app.get('/data', (req, res) => {
